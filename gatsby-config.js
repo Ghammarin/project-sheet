f57d1729a6c57ad965d8010979c5e2b000363817
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-08-11 10:06:27 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,70 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SoundLevel`,
+    description: `Infoblad för varje projekt, data hämtas från Airtable`,
+    author: `Arne`,
   },
+  pathPrefix: `/internt/react_Arnetestar/Gatsby`,
   plugins: [
-    `gatsby-plugin-react-helmet`,
+
+    'gatsby-transformer-remark',
+  
+    {
+  resolve: 'gatsby-source-airtable',
+  options: {
+    apiKey: 'keyEmPlXlN7GLHT4d',
+	tables: [
+          {
+            baseId: `appVntpV9Sejz9kjg`,
+            tableName: `Projekt`,
+            tableView: `Programmeringsvy`,
+			defaultValues: [`KUND`,`Plats`,`KONTAKTPERSON`,`Projektansvarig`,`Personalbokning`,`Bokat`,`Omkostnader`,`ANTECKNINGAR`,`BILAGOR`,`Yearly`],
+            mapping: { 'Personalbokning': `array` },
+			tableLinks: [`Plats`,`Bokat`,`Projektansvarig`,`Personalbokning`,`KONTAKTPERSON`]
+          },
+		  {
+            baseId: `appVntpV9Sejz9kjg`,
+            tableName: `Platser`,
+            tableView: `All`,
+			defaultValues: [`Platsnamn`,`Adress`,`Telefonnummer`,`Attachments`,`Info`],
+//            mapping: { 'ANTECKNINGAR': `text/markdown` },
+			tableLinks: [`Projekt`]
+          },
+		  {
+            baseId: `appVntpV9Sejz9kjg`,
+            tableName: `Bokningsrader`,
+            tableView: `Programmeringsvy`,
+			defaultValues: [`F_rdiga_kit`,`Inventarielista`,`Kommentar`,`Antal_bokat`,`Pris`,`Bokad_utrustning`,`Klart?`,`Antal_i_kit`],
+//            mapping: { 'ANTECKNINGAR': `text/markdown` },
+			tableLinks: [`Projekt`]
+          },
+		  {
+            baseId: `appVntpV9Sejz9kjg`,
+            tableName: `Personal`,
+            tableView: `All`,
+//            mapping: { 'ANTECKNINGAR': `text/markdown` },
+			tableLinks: [`Projekt`]
+          },
+		  {
+            baseId: `appVntpV9Sejz9kjg`,
+            tableName: `Personalbokning`,
+            tableView: `All`,
+			defaultValues: [`Bokad_personal`,`Antal_timmar`,`Starttid`,`Sluttid`],
+//            mapping: { 'ANTECKNINGAR': `text/markdown` },
+			tableLinks: [`Projekt`,`Bokad_personal`]
+          },
+		  {
+            baseId: `appVntpV9Sejz9kjg`,
+            tableName: `Kontaktpersoner`,
+            tableView: `All`,
+//            mapping: { 'ANTECKNINGAR': `text/markdown` },
+			tableLinks: [`Projekt`]
+          },
+
+         ],
+  }
+},
+`gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -30,5 +89,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  
+  ],//plugins
 }
