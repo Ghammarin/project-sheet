173f2eb1ccf44bac61190a6479420b54f1bf55df
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-08-12 12:20:29 +0200

    Update page-template.js

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -3,41 +3,37 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 export default ({ data }) => (
-	var post = data.airtable.data
   <Layout>
   <main>
-        <h3>{post.projekt}</h3><hr/>
+        <h3>{data.airtable.data.projekt}</h3><hr/>
 		</main>
 	<section>
-	<p>Projektansvarig: {post.Projektansvarig[0].data.Namn}</p>
+	<p>Projektansvarig: {data.airtable.data.Projektansvarig[0].data.Namn}</p>
 			<h5>Personal bokad:</h5>
 	
-		<ul>{post.Personalbokning.map((rad, i) =>(
-		<li key={i}>{post.Personalbokning[i].data.Bokad_personal[0].data.Namn}</li>
+		<ul>{data.airtable.data.Personalbokning.map((rad, i) =>(
+		<li key={i}>{data.airtable.data.Personalbokning[i].data.Bokad_personal[0].data.Namn}</li>
 		))}</ul>
 		<h5>Packlista!</h5>
-		<ul>{post.Bokat.map((rad, i) =>(
-		<li key={i}>{post.Bokat[i].data.Bokad_utrustning}</li>
+		<ul>{data.airtable.data.Bokat.map((rad, i) =>(
+		<li key={i}>{data.airtable.data.Bokat[i].data.Bokad_utrustning}</li>
 		))}</ul>
-		<p>{post.Bokat[0].data.Kommentar}</p>
-		<p>{post.ANTECKNINGAR}</p>
+		<p>{data.airtable.data.Bokat[0].data.Kommentar}</p>
+		<p>{data.airtable.data.ANTECKNINGAR}</p>
 		</section>
 		<aside>
 		<h5>Kund:</h5>
-		<div>{post.KONTAKTPERSON.map((person, i) =>(
+		<div>{data.airtable.data.KONTAKTPERSON.map((person, i) =>(
 			<div key={i}>
-				<p>{post.KONTAKTPERSON[i].data.Namn}</p>
-				<p>{post.KONTAKTPERSON[i].data.Telefonnummer}</p>
-				<small>{post.KONTAKTPERSON[i].data.Mailadress}</small>
+				<p>{data.airtable.data.KONTAKTPERSON[i].data.Namn}</p>
+				<p>{data.airtable.data.KONTAKTPERSON[i].data.Telefonnummer}</p>
+				<small>{data.airtable.data.KONTAKTPERSON[i].data.Mailadress}</small>
 			</div> 
 		))}</div>
 		</aside>
 	
   </Layout>
 )
-
-
-
 
 export const query = graphql`
   query GetPage($Path: String!) {
