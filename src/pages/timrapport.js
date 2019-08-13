38853d33bba10eba0eea1ff0abf01f495fa656e9
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-08-14 00:01:45 +0200

    Revert "Delete timrapport.js"
    This reverts commit 6fd7609893ed74bee806fbc6be343e1930cf4089.

---
+++ src/pages/timrapport.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "gatsby"
+
+class Contact extends React.component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleSubmit = e => {
+    console.log(this.state);
+
+    e.preventDefault();
+  }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value})
+}
+  
+    export default ({data}) => (
+      <form>
+        <label>
+          Name
+          <input type="text" name="name" onChange={this.handleChange} />
+        </label>
+        <label>
+          Notes
+          <input type="text" name="notes" onChange={this.handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  
+
