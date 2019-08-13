dc6a405003d6f0ad86828e28fb2f2fad1ef388ce
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-08-14 00:33:44 +0200

    Update timrapport.js
    arghh;

--- src/pages/timrapport.js
+++ src/pages/timrapport.js
@@ -14,7 +14,7 @@ class Contact extends React.Component {
   }
 
   handleChange = e => {
-	  this.setState({ [e.target.name]: e.target.value})
+	  this.setState({ [e.target.name]: e.target.value});
 	}
 
   
