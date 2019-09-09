bbe5928abf7ab3c4e2e0970cbb2ab48f11999af8
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-09-09 14:01:39 +0200

    Update page-template.js

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -16,7 +16,7 @@ export default ({ data }) => (
 		))}</ul>
 		<h5>Packlista!</h5>
 		<ul>{data.airtable.data.Bokat.map((rad, i) =>(
-		<li key={i}>{data.airtable.data.Bokat[i].data.Bokad_utrustning[0]}</li>
+		<li key={i}>{data.airtable.data.Bokat[i].data.Bokad_utrustning[0].data.Bokningsrad}</li>
 		))}</ul>
 		<p>{data.airtable.data.Bokat[0].data.Kommentar}</p>
 		<p>{data.airtable.data.ANTECKNINGAR}</p>
