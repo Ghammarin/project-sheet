2207606d5c41377338d9c83247f2b60678b0c97c
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-10-01 23:10:09 +0200

    Update gatsby-config.js
    wtf2

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
   },
   pathPrefix: `/internt/react_Arnetestar/Gatsby`,
   plugins: [
+  
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
 
     'gatsby-transformer-remark',
   
@@ -72,20 +87,6 @@ module.exports = {
          ],
   }
 },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
 	
 `gatsby-plugin-react-helmet`,
     {
