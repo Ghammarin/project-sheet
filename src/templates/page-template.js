a8c2d99539a8ef0ccb80feb50cb1e7901910bbdf
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-10-01 22:17:07 +0200

    Update page-template.js
    test2

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 export default ({ data }) => (
   <Layout>
   <main>
-        <h3>{data.airtable.data.projekt||""}</h3><hr/>
+        <h3>{data.airtable.data.projekt}</h3><hr/>
 		</main>
 	<section>
 	<p>Projektansvarig: {data.airtable.data.Projektansvarig[0].data.Namn}</p>
@@ -13,7 +13,7 @@ export default ({ data }) => (
 	
 		<ul>{data.airtable.data.Personalbokning.map((rad, i) =>(
 		<li key={i}>{data.airtable.data.Personalbokning[i].data.Bokad_personal[0].data.Namn||""}</li>
-		))}</ul>
+		))||""}</ul>
 		<h5>Packlista!</h5>
 		<ul>{data.airtable.data.Bokat.map((rad, i) =>(
 		<li key={i}>{data.airtable.data.Bokat[i].data.Bokad_utrustning}</li>
