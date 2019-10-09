4048b40c7a96875b8a56ae7ba9e7d53a269b4c5c
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-10-09 22:39:28 +0200

    Update page-template.js
    ta bort test

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -101,4 +101,4 @@ export const query = graphql`
   
 `
 
-<iframe class="airtable-embed" src="https://airtable.com/embed/shrw1edvupHuY6Iuw?backgroundColor=gray" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
+
