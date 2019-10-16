9e3f11a593519a953cf0b06326d60a527b398c7a
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-10-16 22:39:46 +0200

    Update page-template.js
    testa igen

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -51,40 +51,6 @@ export default ({ data }) => (
   </Layout>
   
  
-class Contact extends React.component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  handleSubmit = e => {
-    const fields = {"fields": {"Kommentar": this.state.kommentar, "Notes": this.state.notes}}
-    fetch("https://api.airtable.com/v0/appVntpV9Sejz9kjg/Projekt", {
-      method: "PATCH",
-      headers: {"Authorization": `Bearer ${process.env.AIRTABLE_API}`,
-                "Content-Type": "application/json"},
-      body: JSON.stringify(fields)
-    })
-    .then(() => alert("Form Sent!"))
-    .catch(error => alert(error))
-
-    e.preventDefault();
-  }
-
-  handleChange = e => this.setState({ [e.target.kommentar]: e.target.value})
-
-  render() {
-    return() (
-      <form>
-        <label>
-          Skriv kommentar
-          <input type="text" name="kommentar" onChange={this.handleChange} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    )
-  }
-}
 )
 
 export const query = graphql`
@@ -141,5 +107,39 @@ export const query = graphql`
 }
   
 `
+class Contact extends React.component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleSubmit = e => {
+    const fields = {"fields": {"Kommentar": this.state.kommentar, "Notes": this.state.notes}}
+    fetch("https://api.airtable.com/v0/appVntpV9Sejz9kjg/Projekt", {
+      method: "PATCH",
+      headers: {"Authorization": `Bearer ${process.env.AIRTABLE_API}`,
+                "Content-Type": "application/json"},
+      body: JSON.stringify(fields)
+    })
+    .then(() => alert("Form Sent!"))
+    .catch(error => alert(error))
+
+    e.preventDefault();
+  }
+
+  handleChange = e => this.setState({ [e.target.kommentar]: e.target.value})
+
+  render() {
+    return() (
+      <form>
+        <label>
+          Skriv kommentar
+          <input type="text" name="kommentar" onChange={this.handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
 
 
