0cb1c583a9db92f4cce69f818f3f248f5937958d
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-10-16 21:57:38 +0200

    Update page-template.js
    tagning 2

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -45,6 +45,40 @@ export default ({ data }) => (
 			
 		</section>
 		
+			<form onsubmit="addComment()">
+				Skriv kommentar: <input type="text">
+				<input type="submit">
+			</form>
+			
+			
+			  <script>
+		function addComment() {
+			var Airtable = require('airtable');
+			var base = new Airtable({apiKey: 'keyEmPlXlN7GLHT4d'}).base('appVntpV9Sejz9kjg');
+
+			base('Projekt').update([
+			  {
+				{data.airtable.data.projekt},
+				{data.airtable.data.Kommentar}
+			  },
+			  {
+				{data.airtable.data.projekt},
+				"fields": {
+				  "Kommentar": "2019-10-01",
+				}
+			  }
+			], function(err, records) {
+			  if (err) {
+				console.error(err);
+				return;
+			  }
+			  records.forEach(function(record) {
+				console.log(record.get('id'));
+			  });
+			});
+		}
+	</script>
+		
   </Layout>
   
  
