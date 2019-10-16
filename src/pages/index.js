870883754d190f9376be601fd3f7135632acd23b
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-10-16 23:20:56 +0200

    Update index.js
    hur funkar saker

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,41 @@ const IndexPage = ({ data }) => (
   </Layout>
 )
 
+class Contact extends React.component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleSubmit = e => {
+    const fields = {"fields": {"Kommentar": this.state.kommentar, "Notes": this.state.notes}}
+    fetch("https://api.airtable.com/v0/appVntpV9Sejz9kjg/Projekt", {
+      method: "PATCH",
+      headers: {"Authorization": `Bearer ${process.env.AIRTABLE_API}`,
+                "Content-Type": "application/json"},
+      body: JSON.stringify(fields)
+    })
+    .then(() => alert("Form Sent!"))
+    .catch(error => alert(error))
+
+    e.preventDefault();
+  }
+
+  handleChange = e => this.setState({ [e.target.kommentar]: e.target.value})
+
+  render() {
+    return() (
+      <form>
+        <label>
+          Skriv kommentar
+          <input type="text" name="kommentar" onChange={this.handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
+
 
 // query airtable for the Title and Path of each record,
 // filtering for only records in the Sections table.
