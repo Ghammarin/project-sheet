cb8713a593662f59b3b6031f275d110d7329a9c3
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2019-11-24 16:24:23 +0100

    Update page-template.js
    Nullhantering

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -20,7 +20,7 @@ export default ({ data }) => (
 			</div> 
 		))}</div>
 		</aside>
-			<p>Projektansvarig: {data.airtable.data.Projektansvarig[0].data.Namn}</p>
+			<p>Projektansvarig: {data.airtable.data.Projektansvarig[0]||[].data.Namn}</p>
 			<h5>Personal bokad:</h5>
 				
 			<ul>{(data.airtable.data.Personalbokning||[]).map((rad, i) =>(
