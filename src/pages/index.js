4bc0eff4528f560626b8b973ba23d596b8e4eea8
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2020-02-08 13:52:18 +0100

    Update index.js
    Glömde airtable

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
 
+const airtable = require("airtable");
+
 airtable.configure({
   endpointUrl: "https://api.airtable.com",
   apiKey: "keyEmPlXlN7GLHT4d"
