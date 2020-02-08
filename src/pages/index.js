0e7bef78abccea780546311caed01bee4c54f008
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2020-02-08 14:19:06 +0100

    Update index.js
    testar uppdatera

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react"
-
 import Layout from "../components/layout"
-
 
 const airtable = require("airtable");
 
@@ -12,16 +10,35 @@ airtable.configure({
 
 const base = airtable.base("appE7YBE4hzd7Ew1v");
 
-base("inventarieflik").create({ produkt: "Gabriella", test: "Hejhejhej" }, function(
-  err,
-  record
-) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(record.getId());
-});
+const updateOrInsert = record => {
+  const primaryField = record.experimentVariation;
+
+  base("inventarieflik")
+    .select({
+      maxRecords: 1,
+      view: "Grid view",
+      filterByFormula: `{experimentVariation} = "${primaryField}"`,
+    })
+    .firstPage(function(err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function(r) {
+        console.log("Retrieved", r.get("experimentVariation"));
+        replace(r.id, record);
+      });
+
+      if (!records.length) {
+        console.log("empty");
+        create(record);
+      }
+    });
+};
+
+module.exports = {
+  updateOrInsert,
+};
 
 
 const IndexPage = ({ data }) => (
