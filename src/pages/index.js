6b1dc20f29b20f3d22293450c8022c4b6891b749
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2020-02-08 15:14:12 +0100

    Update index.js
    tillbaka

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,53 +10,16 @@ airtable.configure({
 
 const base = airtable.base("appE7YBE4hzd7Ew1v");
 
-const create = record => {
-  base("lowerFunnel").create({ Name: "Gabriella", test: "test notes" }, function(err, record) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(record.getId());
-  });
-};
-
-const replace = (id, record) => {
-  base("lowerFunnel").replace(id, record, function(err, record) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(record.get("experimentVariation"));
-  });
-};
-
-const updateOrInsert = record => {
-  const primaryField = record.experimentVariation;
-
-  base("inventarieflik")
-    .select({
-      maxRecords: 1,
-      view: "Grid view",
-      filterByFormula: `{experimentVariation} = "${primaryField}"`,
-    })
-    .firstPage(function(err, records) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      records.forEach(function(r) {
-        console.log("Retrieved", r.get("experimentVariation"));
-        replace(r.id, record);
-      });
-
-      if (!records.length) {
-        console.log("empty");
-        create(record);
-      }
-    });
-};
-
-
+base("inventarieflik").create({ produkt: "Gabriella", test: "Hejhejhej" }, function(
+  err,
+  record
+) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(record.getId());
+});
 const IndexPage = ({ data }) => (
   <Layout>
   <h3>Aktuella projekt</h3>
