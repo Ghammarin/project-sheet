65ee131d3319cfeb3712075e6a2d818ce9d669b6
soundleveliuppsalaab <36438122+soundleveliuppsalaab@users.noreply.github.com>
2020-02-08 14:54:46 +0100

    Update index.js
    nytt försök

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import {React} from "react"
-import {Layout} from "../components/layout"
+import React from "react"
+import Layout from "../components/layout"
 
 const airtable = require("airtable");
 
@@ -64,4 +64,4 @@ const IndexPage = ({ data }) => (
 )
 
 
-export default {IndexPage, updateOrInsert}
+export default IndexPage, updateOrInsert;
